87431b9492d6fb0745f66e9efae0adf03de9b59c
dkrolikowski <krolikowski@utexas.edu>
2017-09-10 13:19:17 -0500

    Added orbit plotter and prelim sep/PA calculator to OrbitPredictor class.

--- Q1.py
+++ Q1.py
@@ -5,20 +5,6 @@ import astropy.units as u
 import pdb
 
 def NewtonRaphson( g, gp, x0, tol, args = () ):
-    '''Implementation of the Newton Raphson root finding technique.
-
-    Inputs
-    ---------
-    g    : Name of function you are trying to solve. Takes addition arguments from args.
-    gp   : Name of function defining the derivative of g. Takes arguments from args.
-    x0   : Initial guess for the solution to g.
-    tol  : Percent error tolerance at which to stop solution loop.
-    args : Tuple containing arguments for g and gp.
-
-    Outputs
-    ---------
-    xnew : Solution to g within error tolerance tol.'''
-
     err = 2 * tol # Initialize error to be larger than tolerance
 
     while err > tol:
@@ -29,69 +15,20 @@ def NewtonRaphson( g, gp, x0, tol, args = () ):
     return xnew
 
 def KeplerEq( E, args ):
-    '''Kepler equation in root finding form to determine eccentric anomaly E from mean anomaly M.
-    For input into NewtonRaphson.
-
-    Inputs
-    ---------
-    E    : Eccentric anomaly in radians.
-    args : Tuple containing the eccentricity e and mean anomaly M (in radians).
-
-    Outputs
-    ---------
-    out  : The value of the kepler equation in root finding form.'''
-    
     e, M = args
     
     return E - e * np.sin( E ) - M
 
 def KeplerEqPrime( E, args ):
-    '''Derivative of kepler equation in root finding form for use in NewtonRaphson.
-
-    Inputs
-    ---------
-    E    : Eccentric anomaly in radians.
-    args : Tuple containing the eccentricity e and mean anomaly M (not used here).
-
-    Outputs
-    ---------
-    out  : The value of the derivative of kepler equation in root finding form.'''
-
     e, M = args
 
     return 1.0 - e * np.cos( E )
 
 def xRotMatrix( t ):
-    '''Function to return rotation matrix of angle t about the x axis.
-
-    Inputs
-    ---------
-    t   : Angle of rotation
-
-    Outputs
-    ---------
-    out : 3x3 rotation matrix.'''
-    
     return np.array( [ [ 1, 0, 0 ], [ 0, np.cos(t), - np.sin(t) ], [ 0, np.sin(t), np.cos(t) ] ] )
 
 def zRotMatrix( t ):
-    '''Function to return rotation matrix of angle t about the z axis.
-
-    Inputs
-    ---------
-    t   : Angle of rotation
-
-    Outputs
-    ---------
-    out : 3x3 rotation matrix.'''
-
     return np.array( [ [ np.cos(t), - np.sin(t), 0 ], [ np.sin(t), np.cos(t), 0 ], [ 0, 0, 1 ] ] )
-
-def toObsFrame( posvec, W, I, w ):
-
-    rotmat = np.dot( np.dot( zRotMatrix(W), xRotMatrix(I) ), zRotMatrix(w) )
-
-    return np.dot( rotmat, posvec )
 
 class OrbitPredictor():
 
@@ -117,6 +54,25 @@ class OrbitPredictor():
         if posvec.shape[0] == posvec.size: return np.dot( rotmat, posvec )
         else: return np.array( [ np.dot( rotmat, posvec[:,i] ) for i in range( posvec.shape[1] ) ] ).T
 
+    def retSepPA( self, t ):
+
+        M = 2.0 * np.pi * ( t - self.t0 ) / self.P
+        E = NewtonRaphson( KeplerEq, KeplerEqPrime, M, 1e-9, ( self.e, M ) )
+        f = 2.0 * np.arctan( np.sqrt( ( 1 + self.e ) / ( 1 - self.e ) ) * np.tan( E / 2.0 ) )
+
+        r = self.a * ( 1 - self.e * np.cos( E ) )
+        x = r * np.cos( f )
+        y = r * np.sin( f )
+
+        X, Y, Z = self.toObsFrame( np.array( [ x, y, 0.0 ] ) )
+        R       = np.sqrt( X ** 2.0 + Y ** 2.0 )
+
+        angle = np.arctan2( Y, X ) - np.pi / 2.0
+        if angle < 0.0: PA = np.degrees( angle + 2.0 * np.pi )
+        else:           PA = np.degrees( angle )
+
+        return R, PA    
+        
     def PlotOrbit( self ):
         tarr = np.linspace( 0.0, self.P, 10000 )
         Marr = 2.0 * np.pi * tarr / self.P
@@ -128,37 +84,21 @@ class OrbitPredictor():
         xarr = rarr * np.cos( farr )
         yarr = rarr * np.sin( farr )
 
-        Xarr, Yarr, Zarr = np.array()
+        Xarr, Yarr, Zarr = self.toObsFrame( np.array( [ xarr, yarr, np.zeros( xarr.size ) ] ) )
+        Rarr             = np.sqrt( Xarr ** 2.0 + Yarr ** 2.0 )
+
+        plt.figure()
+        plt.plot( tarr, Rarr, 'k-' )
+        plt.plot( tarr, rarr, 'r--' )
+        for x in [ -1, 1 ]: plt.axhline( y = self.a * ( 1 + x * self.e ), color = 'b', linestyle = ':' )
+
+        plt.figure()
+        plt.plot( Xarr, Yarr, 'k-' )
+        plt.plot( xarr, yarr, 'r--' )
+
+        plt.show()
+
         return None
-        #Xarr, Yarr, Zarr = np.array( [ toObsFrame( np.array( [ xarr[i], yarr[i], 0.0 ] ), self.W, self.I, self.w ) for i in range( xarr.size )
-
-def PlotOrbit( a, e, W, I, w, t0, P ):
-    tarr = np.linspace( 0.0, P, 10000 )
-    Marr = 2.0 * np.pi * tarr / P
-
-    Earr = np.array( [ NewtonRaphson( KeplerEq, KeplerEqPrime, x, 1e-9, ( e, x ) ) for x in Marr ] )
-    farr = 2.0 * np.arctan( np.sqrt( ( 1 + e ) / ( 1 - e ) ) * np.tan( Earr / 2.0 ) )
-
-    rarr = a * ( 1.0 - e * np.cos( Earr ) )
-    xarr = rarr * np.cos( farr )
-    yarr = rarr * np.sin( farr )
-
-    rotmat = np.dot( np.dot( zRotMatrix( W ), xRotMatrix( I ) ), zRotMatrix( w ) )
-    Xarr, Yarr, Zarr = np.array( [ toObsFrame( np.array( [ xarr[i], yarr[i], 0.0 ] ), W, I, w ) for i in range( xarr.size ) ] ).T
-    pdb.set_trace()
-    Rarr = np.sqrt( Xarr ** 2.0 + Yarr ** 2.0 )
-    
-    plt.figure()
-    plt.plot( tarr, Rarr, 'k-' )
-    plt.plot( tarr, rarr, 'r--' )
-    for x in [ -1.0, 1.0 ]:
-        plt.axhline( y = a * ( 1.0 + x * e ), color = 'b', linestyle = ':' )
-
-    plt.figure()
-    plt.plot( Xarr, Yarr, 'k-' )
-    plt.plot( xarr, yarr, 'r--' )
-
-    return None
 
 ##### Question 1
 
@@ -168,26 +108,11 @@ M1 = 1.018; M2 = 4.114 * u.jupiterMass.to('solMass');
 
 P = np.sqrt( a ** 3.0 / ( M1 + M2 ) )
 
-t = 8 * P / 8.0
-M = 2.0 * np.pi * ( t - t0 ) / P
-E = NewtonRaphson( KeplerEq, KeplerEqPrime, M, 1e-9, ( e, M ) )
-f = 2.0 * np.arctan( np.sqrt( ( 1 + e ) / ( 1 - e ) ) * np.tan( E / 2.0 ) )
+t = 2 * P / 5
 
-r = a * ( 1.0 - e * np.cos( E ) )
-x = r * np.cos( f )
-y = r * np.sin( f )
-
-X, Y, Z = toObsFrame( np.array( [ x, y, 0.0 ] ), W, I, w )
-pdb.set_trace()
-R       = np.sqrt( X ** 2.0 + Y ** 2.0 )
-
-angle = np.arctan2( Y, X ) - np.pi / 2.0
-if angle < 0.0:
-    PA = np.degrees( angle + 2 * np.pi )
-else:
-    PA = np.degrees( angle )
-
-PlotOrbit( a, e, W, I, w, t0, P )
+orbit = OrbitPredictor( a, e, W, I, w, t0, M1, M2 )
+r, t  = orbit.retSepPA(t)
+print r, t
 
 # astar = M2 / ( M1 + M2 ) * a
 
