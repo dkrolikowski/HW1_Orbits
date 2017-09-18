9e1758b89e302eab1b89144dfa821834085f9848
dkrolikowski <krolikowski@utexas.edu>
2017-09-17 22:00:42 -0500

    Cleaned up stuff for Q2/3. Added in parallactic motion (which seems to work) but planet still doesn't show up...

--- Q1.py
+++ Q1.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import astropy.units as u
-import matplotlib as mpl
 
 from scipy import signal
 from astropy.time import Time
@@ -9,6 +8,7 @@ from astropy.time import Time
 import pdb
 
 def NewtonRaphson( g, gp, x0, tol, args = () ):
+    
     err = 2 * tol # Initialize error to be larger than tolerance
     while err > tol:
        xnew = x0 - g( x0, args ) / gp( x0, args ) # Calculate next x guess
@@ -18,16 +18,20 @@ def NewtonRaphson( g, gp, x0, tol, args = () ):
     return x0
 
 def KeplerEq( E, args ):
-    e, M = args
+    
+    e, M = args # Read in eccentricity and mean anomaly
     
     return E - e * np.sin( E ) - M
 
 def KeplerEqPrime( E, args ):
-    e, M = args
+    
+    e, M = args # Read in eccentricity and mean anomaly
 
     return 1.0 - e * np.cos( E )
 
 def RotMatrix( t, axis ):
+
+    # Read in t (angle in rad) and axis about which to rotate
     if axis == 'x': return np.array( [ [ 1, 0, 0 ], [ 0, np.cos(t), - np.sin(t) ], [ 0, np.sin(t), np.cos(t) ] ] )
     elif axis == 'z': return np.array( [ [ np.cos(t), - np.sin(t), 0 ], [ np.sin(t), np.cos(t), 0 ], [ 0, 0, 1 ] ] )
 
@@ -116,24 +120,32 @@ class OrbitPredictor():
 
     def getRV( self, t ):
 
-        M   = 2.0 * np.pi * ( t - self.t0 ) / self.P
-        x0  = NewtonRaphson( KeplerEq, KeplerEqPrime, M, 1e-9, ( 0.7, M ) )
-        E   = NewtonRaphson( KeplerEq, KeplerEqPrime, x0, 1e-9, ( self.e, M ) )
-        f   = 2.0 * np.arctan( np.sqrt( ( 1 + self.e ) / ( 1 - self.e ) ) * np.tan( E / 2.0 ) )
+        f, r = self.getf_r( t ) # Just need f as a function of time
 
+        # Negative sign added to A so that moving in the +z direction (towards us) means RV is negative (as per convention)
         A   = - ( 2.0 * np.pi * np.sin( self.I ) ) / ( self.P * np.sqrt( 1 - self.e ** 2.0 ) ) / u.km.to('AU') / u.yr.to('s')
-        RVp = A * self.a2 * ( np.cos( self.w + f ) + self.e * np.cos( self.w ) )
-        RVs = A * self.a1 * ( np.cos( self.ws + f ) + self.e * np.cos( self.ws ) )
+        RVp = A * self.a2 * ( np.cos( self.w + f ) + self.e * np.cos( self.w ) )   # Planet's RV
+        RVs = A * self.a1 * ( np.cos( self.ws + f ) + self.e * np.cos( self.ws ) ) # Star's RV
 
         return RVp, RVs
-                                    
+
+    def FindTransits( self, tarr ):
+
+        X, Y, Z = self.getXYZ( self.a, self.w, tarr )
+        R       = np.sqrt( X ** 2.0 + Y ** 2.0 ) # Calculate projected sep wrt star
+
+        # Look for when the projected sep is less than the stellar radius (ignore planet radius/grazing transits)
+        # AND when the planet is in front of the star
+        transit = np.where( ( R <= self.Rs ) & ( Z > 0.0 ) )[0]
+
+        # Return the times at which there is a transit (re-convert to days)
+        return tarr[transit] * u.yr.to('d')
+
     def PlotOrbit( self ):
 
         tarr = np.linspace( 0.0, self.P, 10000 )
 
         X, Y, Z, x, y = self.getXYZ( self.a, self.w, tarr, retInOrbit = True )
-        R = np.sqrt( X ** 2.0 + Y ** 2.0 )
-        transit = np.where( R <= u.solRad.to('au') )[0]
 
         Xp, Yp, Zp, xp, yp = self.getXYZ( self.a2, self.w, tarr, retInOrbit = True )
         Xs, Ys, Zs, xs, ys = self.getXYZ( self.a1, self.ws, tarr, retInOrbit = True )
@@ -143,73 +155,24 @@ class OrbitPredictor():
 
         plt.figure()
         plt.plot( x, y, 'k-' )
-        ax = plt.gca()
-        ax.add_patch(mpl.patches.Circle((0,0),radius=u.solRad.to('au'),color='r'))
         plt.figure()
-        plt.plot( xp, yp, 'r--' )
-        plt.plot( xp[0], yp[0], 'ro' )
-        plt.plot( xs, ys, 'b--' )
-        plt.plot( xs[0], ys[0], 'bo' )
+        plt.plot( xp, yp, 'r--', xs, ys, 'b--' )
         plt.show()
 
         plt.figure()
         plt.plot( X, Y, 'k-' )
-        ax = plt.gca()
-        ax.add_patch(mpl.patches.Circle((0,0),radius=1*u.solRad.to('au'),color='r'))
-        for t in transit: ax.add_patch( mpl.patches.Circle( (X[t],Y[t]),radius=u.jupiterRad.to('au'), color='b' ) )
         plt.figure()
-        plt.plot( Xp, Yp, 'r--' )
-        plt.plot( Xs, Ys, 'b--' )
+        plt.plot( Xp, Yp, 'r--', Xs, Ys, 'b--' )
         plt.show()
         
         return None
-
-    def PlotRV( self, tarr ):
-
-        RVp, RVs = np.array( [ self.getRV( t ) for t in tarr ] ).T
-        tarrplot = tarr * u.yr.to('d') - 2451900.0
-
-        plt.clf()
-        fig, ( axs, axp ) = plt.subplots( 2, 1, sharex = True )
-
-        axs.plot( tarrplot, RVs, 'k-' )
-        axs.set_ylabel( 'Stellar Velocity (km/s)' )
-
-        axp.plot( tarrplot, RVp, 'r-' )
-        axp.set_ylabel( 'Planet Velocity (km/s)' )
-        axp.set_xlabel( 'JD - 2451900' )
-
-        fig.subplots_adjust( hspace = 0 )
-        fig.suptitle( 'HD 80606 Radial Velocity Curves' )
-
-        plt.savefig('Plots/HD80606_RV.pdf')
-
-        plt.clf()
-        plt.plot( tarrplot, RVs, 'k-' )
-        plt.axhline( y = 0.0, color = 'r', ls = ':' )
-        plt.ylabel( 'Radial Velocity (km/s)' )
-        plt.xlabel( 'JD - 2451900' )
-        plt.title( 'HD 80606 Stellar Radial Velocity Curve' )
-        plt.savefig( 'Plots/HD80606_StarRV.pdf' )
-        plt.clf()
-        
-        return RVp, RVs
-
-    def FindTransits( self, tarr ):
-
-        X, Y, Z = self.getXYZ( self.a, self.w, tarr / u.yr.to('d') )
-        R       = np.sqrt( X ** 2.0 + Y ** 2.0 )
-
-        transit = np.where( ( R <= self.Rs ) & ( Z > 0.0 ) )[0]
-
-        return tarr[transit]
     
 ##### Question 2, 3, 4 -- HD 80606 #####
 a     = 0.4564 # AU
 e     = 0.934
-W     = 160.98 * np.pi / 180.0 # Rad
-I     = 89.232 * np.pi / 180.0 # Rad
-w     = 300.77 * np.pi / 180.0 # Rad
+W     = np.radians( 160.98 ) # Rad
+I     = np.radians( 89.232 ) # Rad
+w     = np.radians( 300.77 ) # Rad
 t0    = 2451973.72 / u.yr.to('d') # Year
 M1    = 1.018 # Sol Mass
 M2    = 4.114 * u.jupiterMass.to('solMass') # Sol Mass
@@ -219,26 +182,36 @@ P     = np.sqrt( a ** 3.0 / ( M1 + M2 ) )
 
 doQ2 = False
 doQ3 = False
+doQ4 = False
 
 # Get class instance for HD 80606 orbit
 orbit = OrbitPredictor( a, e, W, I, w, t0, M1, M2, Rs, p )
-#orbit.PlotOrbit() # Show orbits in orbit frame (both relative and absolute)
+
+# orbit.PlotOrbit() # Show orbits in orbit frame (both relative and absolute)
 
 ### Question 2 - Radial Velocity ###
 
 if doQ2:
-    # Set dates to compute RV for
+    # Set dates to compute RV for in units of days
     aug1     = Time( '2017-08-01 00:00:00', scale = 'utc' ).jd
     jan1     = Time( '2018-01-01 00:00:00', scale = 'utc' ).jd
     tarr     = np.linspace( aug1, jan1, 100000 )
 
-    # Plot and return planet and stellar RVs
-    RVp, RVs = orbit.PlotRV( tarr / u.yr.to('d') )
+    # Get planet and stellar RVs
+    RVp, RVs = orbit.getRV( tarr * u.d.to('yr') ) # Give tarr in yrs as that's what is used
 
-    mins     = signal.argrelmin( RVs )[0]
-    maxs     = signal.argrelmax( RVs )[0]
+    plt.clf()
+    plt.plot( tarr - 2400000.5, RVs, 'k-' )
+    plt.axhline( y = 0.0, color = 'r', ls = ':' )
+    plt.ylabel( 'Radial Velocity (km/s)' )
+    plt.xlabel( 'MJD (JD - 2400000.5)' )
+    plt.savefig( 'Plots/HD80606_StarRV.pdf' )
 
-    print 'RV minima occur at: '
+    # Now find mins and maxs of the radial velocity curve
+    mins = signal.argrelmin( RVs )[0] # mins
+    maxs = signal.argrelmax( RVs )[0] # maxs
+
+    print '\nRV minima occur at: '
     print Time( tarr[mins], format = 'jd' ).iso
 
     print '\nRV maxima occur at: '
@@ -247,35 +220,105 @@ if doQ2:
 ### Question 3 - Transits ###
 
 if doQ3:
-    transits = orbit.FindTransits( tarr )
+    # Set dates to search for transits over in units of days (same as Q2)
+    aug1     = Time( '2017-08-01 00:00:00', scale = 'utc' ).jd
+    jan1     = Time( '2018-01-01 00:00:00', scale = 'utc' ).jd
+    tarr     = np.linspace( aug1, jan1, 100000 )
+
+    transits = orbit.FindTransits( tarr * u.d.to('yr') ) # Give tarr in yrs as that's what is used
+    
+    # Split the transit time array into each separate transit
     transits = np.split( transits, np.where( np.diff( transits ) > 10.0 * np.median( np.diff( transits ) ) )[0] + 1 )
+
+    # Print out start, mid, and end time of transit in UTC
     for t in transits:
+        print '\n'
         print Time( t.min(), format = 'jd' ).iso
         print Time( np.median(t), format = 'jd' ).iso
         print Time( t.max(), format = 'jd' ).iso
-        print '\n'
 
-dec  = 50 + 36.0 / 60.0 + 13.43 / 3600.0
-ra   = 15 * ( 9 + 22.0 / 60.0 + 37.5769 / 3600.0 )
-pmas = 45.76 * u.mas.to('deg') * u.d.to('yr')
-pma  = pmas / np.cos( dec * np.pi / 180.0 )
-pmd  = 16.56 * u.mas.to('deg') * u.d.to('yr')
+def RADec_Ecliptic( ra, dec ):
 
-aug1     = Time( '2017-08-01 00:00:00', scale = 'utc' ).jd
-aug5     = Time( '2023-08-01 00:00:00', scale = 'utc' ).jd
-tarr     = np.linspace( aug1, aug5, 100000 ) - aug1
+    eps = 23.43699 * np.pi / 180.0
 
-X, Y, Z = orbit.getXYZ( orbit.a1, orbit.ws, tarr / u.yr.to('d') )
-X = X / orbit.d * u.rad.to('deg')
-Y = Y / orbit.d * u.rad.to('deg')
+    lat = np.arcsin( np.sin(dec) * np.cos(eps) - np.cos(dec) * np.sin(eps) * np.sin(ra) )
+    lon = np.arccos( np.cos(ra) * np.cos(dec) / np.cos(lat) )
 
-CoMx = ra + pma * tarr * u.d.to('yr')
-CoMy = dec + pmd * tarr * u.d.to('yr')
+    return lon, lat
 
-totalx = ( X + CoMx - ra ) * u.deg.to('mas')
-totaly = ( Y + CoMy - dec ) * u.deg.to('mas')
+def Ecliptic_RADec( lon, lat ):
+
+    eps = 23.43699 * np.pi / 180.0
+
+    dec = np.arcsin( np.sin(lat) * np.cos(eps) + np.cos(lat) * np.sin(eps) * np.sin(lon) )
+    ra  = np.arccos( np.cos(lon) * np.cos(lat) / np.cos(dec) )
+
+    return ra, dec
+
+def getSolLon( jd ):
+
+    N   = jd - 2451545.0;
+    L   = ( 280.460 + 0.9856474 * N ) % 360.0
+    g   = ( 357.528 + 0.9856003 * N ) % 360.0
+
+    lon = ( L + 1.915 * np.sin( g * np.pi / 180.0 ) + 0.02 * np.sin( g * np.pi / 90.0 ) ) % 360.0
+
+    return np.radians( lon )
+
+ra   = np.radians( 15 * ( 9 + 22.0 / 60.0 + 37.5769 / 3600.0 ) )
+dec  = np.radians( 50 + 36.0 / 60.0 + 13.43 / 3600.0 )
+
+pma = 45.76 * u.mas.to('rad') * u.d.to('yr') # Convert pm from mas/yr to rad/day
+#pma  = pma / np.cos( dec )                   # Go from mua cosd to mua (do I need this???)
+pmd  = 16.56 * u.mas.to('rad') * u.d.to('yr') # Convert pm from mas/yr to rad/day
+
+# Set array of time over which to calculate astrometric motion in days
+aug1   = Time( '2017-08-01 00:00:00', scale = 'utc' ).jd
+aug5   = Time( '2022-08-01 00:00:00', scale = 'utc' ).jd
+tarr   = np.linspace( aug1, aug5, 100000 )
+p      = p * u.arcsec.to('rad')
+
+# Calculate motion due to parallax
+sollon = getSolLon( tarr ) # Get the ecliptic longitude of the sun as a function of time
+
+lon, lat = RADec_Ecliptic( ra, dec ) # Get ecliptic coordinates of the star
+dellon   = p * np.sin( sollon - lon ) / np.cos( lat )
+dellat   = - p * np.sin( lat ) * np.cos( sollon - lon )
+
+lon += dellon
+lat += dellat
+
+raoft, decoft = Ecliptic_RADec( lon, lat )
+
+raoft  += pma * ( tarr - tarr[0] ) - ra
+decoft += pmd * ( tarr - tarr[0] ) - dec
 
 plt.clf()
-plt.plot( totalx, totaly )
-plt.plot( (CoMx - ra)*u.deg.to('mas'), (CoMy - dec)*u.deg.to('mas') )
+plt.plot( raoft * u.rad.to('mas'), decoft * u.rad.to('mas'), 'k-' )
 plt.show()
+
+X, Y, Z = orbit.getXYZ( orbit.a1, orbit.ws, tarr * u.d.to('yr') )
+X = X / orbit.d
+Y = Y / orbit.d
+
+raoft += X
+decoft += Y
+
+#plt.clf()
+#plt.plot( raoft * u.rad.to('mas'), decoft * u.rad.to('mas'), 'k-' )
+#plt.show()
+
+# raarr += X
+# decarr += Y
+
+
+# CoMx = ra + pma * tarr * u.d.to('yr')
+# CoMy = dec + pmd * tarr * u.d.to('yr')
+
+# totalx = ( X + CoMx - ra ) * u.deg.to('mas')
+# totaly = ( Y + CoMy - dec ) * u.deg.to('mas')
+
+# plt.clf()
+# plt.plot( totalx, totaly )
+# plt.plot( (CoMx - ra)*u.deg.to('mas'), (CoMy - dec)*u.deg.to('mas') )
+# plt.show()
