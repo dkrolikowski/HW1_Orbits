92e9a47dc806dd0fee1b432335439954680d6fd6
dkrolikowski <krolikowski@utexas.edu>
2017-09-18 11:39:56 -0500

    Added other system as test for astrometry.

--- Q1.py
+++ Q1.py
@@ -46,7 +46,7 @@ def getPA( x, y ):
 
 class OrbitPredictor():
 
-    def __init__( self, a, e, W, I, w, t0, M1, M2, Rs, p ):
+    def __init__( self, a, e, W, I, w, t0, M1, M2, Rs, Rp, p ):
         self.a  = a  # Semimajor axis planet wrt star
         self.e  = e  # Eccentricity
         self.W  = W  # Longitude of ascending node
@@ -56,6 +56,7 @@ class OrbitPredictor():
         self.M1 = M1 # Mass of star
         self.M2 = M2 # Mass of planet
         self.Rs = Rs # Radius of star
+        self.Rp = Rp # Radius of planet
         self.p  = p  # Parallax of system
 
         self.ws = w - np.pi # w of star, offset from planet by pi
@@ -70,13 +71,13 @@ class OrbitPredictor():
         M = 2.0 * np.pi * ( t - self.t0 ) / self.P
 
         if np.isscalar( M ):
-            if e > 0.8: x0 = NewtonRaphson( KeplerEq, KeplerEqPrime, M, 1e-9, ( 0.75, M ) )
+            if self.e > 0.8: x0 = NewtonRaphson( KeplerEq, KeplerEqPrime, M, 1e-9, ( 0.75, M ) )
             else:       x0 = M
 
             E = NewtonRaphson( KeplerEq, KeplerEqPrime, x0, 1e-9, ( self.e, M ) )
             
         else:
-            if e > 0.8: x0 = np.array( [ NewtonRaphson( KeplerEq, KeplerEqPrime, x, 1e-9, ( 0.75, x ) ) for x in M ] )
+            if self.e > 0.8: x0 = np.array( [ NewtonRaphson( KeplerEq, KeplerEqPrime, x, 1e-9, ( 0.75, x ) ) for x in M ] )
             else:       x0 = M
 
             E = np.array( [ NewtonRaphson( KeplerEq, KeplerEqPrime, x0[i], 1e-9, ( self.e, M[i] ) ) for i in range( M.size ) ] )
@@ -134,9 +135,9 @@ class OrbitPredictor():
         X, Y, Z = self.getXYZ( self.a, self.w, tarr )
         R       = np.sqrt( X ** 2.0 + Y ** 2.0 ) # Calculate projected sep wrt star
 
-        # Look for when the projected sep is less than the stellar radius (ignore planet radius/grazing transits)
-        # AND when the planet is in front of the star
-        transit = np.where( ( R <= self.Rs ) & ( Z > 0.0 ) )[0]
+        # Look for when the projected sep is less than the stellar radius + planet radius (would account for grazing transits)
+        # AND when the planet is in front of the star ( z > 0.0 )
+        transit = np.where( ( R <= self.Rs + self.Rp ) & ( Z > 0.0 ) )[0]
 
         # Return the times at which there is a transit (re-convert to days)
         return tarr[transit] * u.yr.to('d')
@@ -177,15 +178,31 @@ t0    = 2451973.72 / u.yr.to('d') # Year
 M1    = 1.018 # Sol Mass
 M2    = 4.114 * u.jupiterMass.to('solMass') # Sol Mass
 Rs    = 1.037 * u.solRad.to('au') # AU
+Rp    = 1.029 * u.jupiterRad.to('au')
 p     = 15.3e-3 # Arcsec
 P     = np.sqrt( a ** 3.0 / ( M1 + M2 ) )
+
+a     = 0.213 # AU
+e     = 0.1
+W     = np.radians( 25 ) # Rad
+I     = np.radians( 84 ) # Rad
+w     = np.radians( 339 ) # Rad
+t0    = 2451973.72 / u.yr.to('d') # Year
+M1    = 0.32 # Sol Mass
+M2    = 1.9 * u.jupiterMass.to('solMass') # Sol Mass
+Rs    = 1.037 * u.solRad.to('au') # AU
+Rp    = 1.029 * u.jupiterRad.to('au')
+p     = 215e-3 # Arcsec
+P     = np.sqrt( a ** 3.0 / ( M1 + M2 ) )
+
+d = 1 / p * u.pc.to('au')
 
 doQ2 = False
 doQ3 = False
 doQ4 = False
 
 # Get class instance for HD 80606 orbit
-orbit = OrbitPredictor( a, e, W, I, w, t0, M1, M2, Rs, p )
+orbit = OrbitPredictor( a, e, W, I, w, t0, M1, M2, Rs, Rp, p )
 
 # orbit.PlotOrbit() # Show orbits in orbit frame (both relative and absolute)
 
@@ -268,8 +285,10 @@ def getSolLon( jd ):
 ra   = np.radians( 15 * ( 9 + 22.0 / 60.0 + 37.5769 / 3600.0 ) )
 dec  = np.radians( 50 + 36.0 / 60.0 + 13.43 / 3600.0 )
 
+lon, lat = RADec_Ecliptic( ra, dec ) # Get ecliptic coordinates of the star
+
 pma = 45.76 * u.mas.to('rad') * u.d.to('yr') # Convert pm from mas/yr to rad/day
-#pma  = pma / np.cos( dec )                   # Go from mua cosd to mua (do I need this???)
+pma  = pma / np.cos( dec )                   # Go from mua cosd to mua (do I need this???)
 pmd  = 16.56 * u.mas.to('rad') * u.d.to('yr') # Convert pm from mas/yr to rad/day
 
 # Set array of time over which to calculate astrometric motion in days
@@ -278,10 +297,26 @@ aug5   = Time( '2022-08-01 00:00:00', scale = 'utc' ).jd
 tarr   = np.linspace( aug1, aug5, 100000 )
 p      = p * u.arcsec.to('rad')
 
+obstarr   = np.sort( np.random.uniform( aug1, aug5, 100 ) )
+obssollon = getSolLon( obstarr )
+obsdellon = p * np.sin( obssollon - lon ) / np.cos( lat )
+obsdellat = - p * np.sin( lat ) * np.cos( obssollon - lon )
+
+obslon = lon + obsdellon
+obslat = lat + obsdellat
+
+obsra, obsdec = Ecliptic_RADec( obslon, obslat )
+obsra += pma * ( obstarr - tarr[0] ) - ra
+obsdec += pmd * ( obstarr - tarr[0] ) - dec
+
+sigma = 0.2 * u.mas.to('rad')
+
+obsra += np.random.normal( 0.0, sigma, 100 )
+obsdec += np.random.normal( 0.0, sigma, 100 )
+
 # Calculate motion due to parallax
 sollon = getSolLon( tarr ) # Get the ecliptic longitude of the sun as a function of time
 
-lon, lat = RADec_Ecliptic( ra, dec ) # Get ecliptic coordinates of the star
 dellon   = p * np.sin( sollon - lon ) / np.cos( lat )
 dellat   = - p * np.sin( lat ) * np.cos( sollon - lon )
 
@@ -290,19 +325,32 @@ lat += dellat
 
 raoft, decoft = Ecliptic_RADec( lon, lat )
 
-raoft  += pma * ( tarr - tarr[0] ) - ra
-decoft += pmd * ( tarr - tarr[0] ) - dec
+rapmt  = pma * ( tarr - tarr[0] )
+decpmt = pmd * ( tarr - tarr[0] )
+
+raoft  += rapmt - ra
+decoft += decpmt - dec
 
 plt.clf()
-plt.plot( raoft * u.rad.to('mas'), decoft * u.rad.to('mas'), 'k-' )
+plt.plot( raoft * u.rad.to('mas'), decoft * u.rad.to('mas'), 'k--' )
+plt.plot( rapmt * u.rad.to('mas'), decpmt * u.rad.to('mas'), 'r:' )
+plt.errorbar( obsra * u.rad.to('mas'), obsdec * u.rad.to('mas'), xerr = sigma * u.rad.to('mas'), yerr = sigma * u.rad.to('mas'), fmt = 'k.', ls = 'none' )
 plt.show()
 
 X, Y, Z = orbit.getXYZ( orbit.a1, orbit.ws, tarr * u.d.to('yr') )
 X = X / orbit.d
 Y = Y / orbit.d
 
-raoft += X
-decoft += Y
+raoft += Y
+decoft += X
+
+plt.clf()
+plt.plot( X * u.rad.to('mas'), Y * u.rad.to('mas'), 'k-' )
+plt.show()
+
+plt.clf()
+plt.plot( raoft * u.rad.to('mas'), decoft * u.rad.to('mas'), 'k-' )
+plt.show()
 
 #plt.clf()
 #plt.plot( raoft * u.rad.to('mas'), decoft * u.rad.to('mas'), 'k-' )
